5031ba30c957f5e1797c3e49c90a99cfc3409c4e
bhosaleparas <parasbhosale375@gmail.com>
2025-12-27 00:17:42 +0530

    # added Authentication functions

--- auth.py
+++ auth.py
@@ -7,6 +7,7 @@ import schemas
 from database import get_db
 from fastapi.security import OAuth2PasswordBearer
 from jose import JWTError, jwt
+import crud
 
 
 
@@ -42,3 +43,34 @@ def create_access_token(data:dict,expires_delta:Optional[timedelta]=None):
     payload.update({"exp":expire})
     encoded_jwt=jwt.encode(payload,SECRET_KEY,algorithm=ALGORITHM)
     return encoded_jwt
+
+
+
+async def get_current_user(token: str = Depends(oauth2_scheme), db: Session = Depends(get_db)):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+    )
+    
+    try:
+        payload=jwt.decode(token,SECRET_KEY,algorithms=[ALGORITHM])
+        username:str=payload.get("sub")
+        if username is None:
+            raise credentials_exception
+        
+        token_data=schemas.TokenDate(username=username)
+    
+    except:
+        raise credentials_exception
+    
+    user=crud.get_user_by_username(db,username=token_data.username)
+    if user is None:
+        raise credentials_exception
+
+    return user
+
+
+async def get_current_active_user(current_user: schemas.User = Depends(get_current_user)):
+    if not current_user:
+        raise HTTPException(status_code=400, detail="Inactive user")
+    return current_user
