0a1b3dfcac14af6b83bb044146133d5d99e0719a
bhosaleparas <parasbhosale375@gmail.com>
2025-12-26 17:42:38 +0530

    password hash scheme changes from bcrypt to argon2

--- auth.py
+++ auth.py
@@ -9,7 +9,7 @@ from fastapi.security import OAuth2PasswordBearer
 
 
 
-pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+pwd_context = CryptContext(schemes=["argon2"], deprecated="auto")
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
 
 
@@ -17,7 +17,7 @@ oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
 def verify_password(plain_password,hashed_password):
     return pwd_context.verify(plain_password,hashed_password)
 
-def get_hashed_password(plain_pasword):
+def get_password_hash(plain_pasword):
     return pwd_context.hash(plain_pasword)
 
 
